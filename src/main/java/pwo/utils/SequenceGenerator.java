3bfcf7e7143fe53a9b2a4af250d096a1fecd6a9d
Uladzislau Dzerzhanovich <vderzhanovich@gmail.com>
2023-10-25 11:35:11 +0200

    Wykonanie 3 zadań z przykładowymi wynikami

---
+++ src/main/java/pwo/utils/SequenceGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pwo.utils;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author student
+ */
+public interface SequenceGenerator {
+
+    void reset();
+
+    BigDecimal nextTerm();
+
+    BigDecimal getTerm(int i);
+}
